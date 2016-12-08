f26d33c5d5c775b88ff7c49b45357fedc401cbae
Jackmrzhou <594780735@qq.com>
2016-12-08 19:31:28 +0800

    fix somebug

--- anothergame.py
+++ anothergame.py
@@ -1,8 +1,6 @@
 # 有很多能大量减少代码量的地方为什么我不优化？
 # 因为我写了几处才意识到-。-!!  就懒得改了
 import random
-
-
 class livingthing(object):
     def __init__(self, name, HP, MP, backpack, defend, attack):
         self.name = name
@@ -135,7 +133,8 @@ class livingthing(object):
         get_monster_HP = The_monster.HP
         round_fight = 1
         while True:
-            print(round_fight)
+            print(' ')
+            print('Round:%s' % round_fight)
             Hero.hero_detail()
             The_monster.monsters_detail()
             print('You can use pills in your backpack,or attck the monster!')
@@ -169,8 +168,6 @@ class livingthing(object):
                 The_monster.HP -= Hero.attack * The_monster.defend // 100
                 Hero.judge_HP()
                 The_monster.judge_HP()
-                Hero.hero_detail()
-                The_monster.monsters_detail()
                 if Hero.dead == True or The_monster.dead == True:
                     The_monster.dead = False
                     The_monster.HP = get_monster_HP
@@ -330,6 +327,8 @@ Hero.hero_detail()
 
 while floors.floor != 20:
     print('This is floor %s' % floors.floor)
+    Hero.hero_detail()
+
     if floors.floor <= 15:
         choose_room = random.randint(0, 4)
         room = rooms[choose_room]
@@ -354,25 +353,29 @@ while floors.floor != 20:
     if choice == 2:
         pass
     elif choice == 1:
-        print('Use the stuff in order!')
-        while True:
-            choose_stuff = int(input())
-            if choose_stuff == '*':
-                break
-            elif Hero.backpack[choose_stuff] == 'reborn_pill':
-                print('You input a wrong number or you can not use that stuff!')
-                print('Input again or input * to not use backpack!')
-            elif choose_stuff > len(Hero.backpack):
-                print('You input a wrong number or you can not use that stuff!')
-                print('Input again or input * to not use backpack!')
-            else:
-                break
-        if choose_stuff != '*':
-            Hero.use_stuff(choose_stuff)
+        if len(Hero.backpack) != 0:
+            print('Use the stuff in order!')
+            print(Hero.backpack)
+            while True:
+                choose_stuff = input()
+                if choose_stuff == '*':
+                    break
+                elif int(choose_stuff) > len(Hero.backpack):
+                    print('You input a wrong number or you can not use that stuff!')
+                    print('Input again or input * to not use backpack!')
+                elif Hero.backpack[int(choose_stuff)] == 'reborn_pill':
+                    print('You input a wrong number or you can not use that stuff!')
+                    print('Input again or input * to not use backpack!')
+                else:
+                    break
+            if choose_stuff != '*':  
+                Hero.use_stuff(int(choose_stuff))
+        else:
+            print('The backpack do not have anything!')
     print('There 5 rooms before you.Please choose one!')
 
     # 其实room根本就不是你选的（滑稽）
-    # 但是我还是要判断用户有没有合法输入  ！——。——！
+    #但是我还是要判断用户有没有合法输入  ！——。——！
     while True:
         nonsense = int(input())
         if nonsense >= 1 and nonsense <= 5:
@@ -441,9 +444,10 @@ while floors.floor != 20:
                     Hero.equip_weapon(box_stuff)
                 print('Input anything to go to next floor!')
                 nonsense = input()
-
+            
             else:
                 Hero.backpack.append(box_stuff)
+                print('You get %s' % box_stuff)
                 print('Input anything to go to next floor!')
                 nonsense = input()
 
@@ -496,101 +500,101 @@ print('                                                      Made by Jack_Zhou')
 
 '''
 Once I was seven years old my momma told me
-	那年我七岁 妈妈就对我说
+    那年我七岁 妈妈就对我说
 Go make yourself some friends or you'll be lonely
-	去交些朋友 不然你会孤独寂寞
+    去交些朋友 不然你会孤独寂寞
 Once I was seven years old
-	那年我七岁
+    那年我七岁
 It was a big big world but we thought we were bigger
-	这个辽阔的大千世界 总以为我们也会变得更加强大
+    这个辽阔的大千世界 总以为我们也会变得更加强大
 Pushing each other to the limits we were learning quicker
-	将彼此逼到绝境 我们得以更快的成长
+    将彼此逼到绝境 我们得以更快的成长
 By eleven smoking herb and drinking burning liquor
-	十一岁那年 我吸大麻 喝烈性酒
+    十一岁那年 我吸大麻 喝烈性酒
 Never rich so we were out to make that steady figure
-	生活捉襟见肘 我们离家奋斗只为有个稳定收入
+    生活捉襟见肘 我们离家奋斗只为有个稳定收入
 Once I was eleven years old my daddy told me
-	十一岁那年 爸爸对我说
+    十一岁那年 爸爸对我说
 Go get yourself a wife or you'll be lonely
-	给自己找个妻子 否则你会空虚寂寞
+    给自己找个妻子 否则你会空虚寂寞
 Once I was eleven years old
-	那年我十一岁
+    那年我十一岁
 I always had that dream like my daddy before me
-	我常梦想着有朝一日能像爸爸一样成为一个歌手
+    我常梦想着有朝一日能像爸爸一样成为一个歌手
 So I started writing songs I started writing stories
-	所以我开始写歌 开始写不同的故事
+    所以我开始写歌 开始写不同的故事
 Something about the glory just always seemed to bore me
-	曾经的光辉岁月 对我来说也已厌倦
+    曾经的光辉岁月 对我来说也已厌倦
 Cause only those I really love will ever really know me
-	因为只有那些我真正爱的人才真的懂我
+    因为只有那些我真正爱的人才真的懂我
 Once I was 20 years old my story got told
-	二十岁那年 我的故事广为流传
+    二十岁那年 我的故事广为流传
 Before the morning sun when life was lonely
-	黎明还未照耀前 孤独的我无人相伴
+    黎明还未照耀前 孤独的我无人相伴
 Once I was 20 years old
-	那年我二十岁
+    那年我二十岁
 I only see my goals I don't believe in failure
-	我一心实现梦想 从不相信失败
+    我一心实现梦想 从不相信失败
 Cause I know the smallest voices they can make it major
-	因为我知道蝼蚁也能成就不朽
+    因为我知道蝼蚁也能成就不朽
 I got my boys with me at least those in favor
-	我找到一群志同道合的伙伴
+    我找到一群志同道合的伙伴
 And if we don't meet before I leave I hope I'll see you later
-	如果我离开前 我们无缘碰面 就让我们后会有期
+    如果我离开前 我们无缘碰面 就让我们后会有期
 Once I was 20 years old my story got told
-	二十岁那年 我的故事广为流传
+    二十岁那年 我的故事广为流传
 I was writing about everything I saw before me
-	我写下每一个亲眼所见的故事
+    我写下每一个亲眼所见的故事
 Once I was 20 years old
-	那年我二十岁
+    那年我二十岁
 Soon we'll be 30 years old our songs have been sold
-	很快我们三十而立 我们的歌也是人尽皆知
+    很快我们三十而立 我们的歌也是人尽皆知
 We've traveled around the world and we're still rolling
-	我们游遍世界 我们不曾止步
+    我们游遍世界 我们不曾止步
 Soon we'll be 30 years old
-	很快我们就三十了
+    很快我们就三十了
 I'm still learning about life
-	我依旧在领悟人生真谛
+    我依旧在领悟人生真谛
 My woman brought children for me
-	爱人为我生了几个孩子
+    爱人为我生了几个孩子
 So I can sing them all my songs
-	所以我可以为他们唱我的歌
+    所以我可以为他们唱我的歌
 And I can tell them stories
-	我可以为他们讲我的故事
+    我可以为他们讲我的故事
 Most of my boys are with me
-	昔日兄弟仍跟有联络
+    昔日兄弟仍跟有联络
 Some are still out seeking glory
-	有些仍在追名逐利
+    有些仍在追名逐利
 And some I had to leave behind
-	有些已被我遗忘脑海
+    有些已被我遗忘脑海
 My brother I'm still sorry
-	兄弟们 我仍心怀歉意
+    兄弟们 我仍心怀歉意
 Soon I'll be 60 years old my daddy got 61
-	很快我就年过花甲 我父亲也年逾古稀
+    很快我就年过花甲 我父亲也年逾古稀
 Remember life and then your life becomes a better one
-	时刻感恩 生活就会更加美好
+    时刻感恩 生活就会更加美好
 I made the man so happy when I wrote a letter once
-	又一次给老爸写信 他高兴不已
+    又一次给老爸写信 他高兴不已
 I hope my children come and visit once or twice a month
-	我希望我的孩子偶尔也来看望一下我
+    我希望我的孩子偶尔也来看望一下我
 Soon I'll be 60 years old will I think the world is cold
-	很快我将年逾六十 不知是否还会觉得人世冷漠
+    很快我将年逾六十 不知是否还会觉得人世冷漠
 Or will I have a lot of children who can warm me
-	还是会拥有一群温暖我心的孩子
+    还是会拥有一群温暖我心的孩子
 Soon I'll be 60 years old
-	#很快我将年逾六十
+    #很快我将年逾六十
 Soon I'll be 60 years old will I think the world is cold
-	很快我将年逾六十 不知是否还会觉得人世冷漠
+    很快我将年逾六十 不知是否还会觉得人世冷漠
 Or will I have a lot of children who can warm me
-	还是拥有一群温暖我心的孩子
+    还是拥有一群温暖我心的孩子
 Soon I'll be 60 years old
-	#很快我将年逾六十
+    #很快我将年逾六十
 Once I was seven years old my momma told me
-	那年我七岁 妈妈就对我说
+    那年我七岁 妈妈就对我说
 Go make yourself some friends or you'll be lonely
-	去交些朋友 不然你会孤独寂寞
+    去交些朋友 不然你会孤独寂寞
 Once I was seven years old
-	那年我七岁
+    那年我七岁
 Once I was seven years old
-	那年我七岁
+    那年我七岁
 '''
